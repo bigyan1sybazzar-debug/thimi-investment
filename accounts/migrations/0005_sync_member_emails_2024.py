a832b6c501285d66c7a341bca372dbd2b2e1cca5
biggs111 <95580387+biggs111@users.noreply.github.com>
2026-08-13 17:09:08 +0545

    feat: data migration 0005 to sync all member emails on Railway production DB

---
+++ accounts/migrations/0005_sync_member_emails_2024.py
@@ -0,0 +1,56 @@
+from django.db import migrations
+
+MEMBER_EMAILS = {
+    'M0001': ('atlantic@thimi.com',     'atlantic@thimi.com'),
+    'M0002': ('bigyan.neupane6@gmail.com', 'bigyan.neupane6@gmail.com'),
+    'M0003': ('bkalpa.khadka@gmail.com',  'bkalpa.khadka@gmail.com'),
+    'M0004': ('bishalpandey32166@gmail.com', 'bishalpandey32166@gmail.com'),
+    'M0005': ('devendra@thimi.com',     'devendra@thimi.com'),
+    'M0006': ('dipin@thimi.com',        'dipin@thimi.com'),
+    'M0007': ('nishan@thimi.com',       'nishan@thimi.com'),
+    'M0008': ('mepaone3@gmail.com',     'mepaone3@gmail.com'),
+    'M0009': ('prabin@thimi.com',       'prabin@thimi.com'),
+    'M0010': ('Pradeeptandan40@gmail.com', 'Pradeeptandan40@gmail.com'),
+    'M0011': ('prashant@thimi.com',     'prashant@thimi.com'),
+    'M0012': ('pratik@thimi.com',       'pratik@thimi.com'),
+    'M0013': ('sandeshupadhya1@gmail.com', 'sandeshupadhya1@gmail.com'),
+    'M0014': ('vikram@thimi.com',       'vikram@thimi.com'),
+    'M0015': ('dahalyuben@gmail.com',   'dahalyuben@gmail.com'),
+}
+
+
+def sync_emails(apps, schema_editor):
+    Member = apps.get_model('accounts', 'Member')
+    User = apps.get_model('auth', 'User')
+
+    for member_id, (new_email, new_username) in MEMBER_EMAILS.items():
+        try:
+            member = Member.objects.select_related('user').get(member_id=member_id)
+            user = member.user
+            changed = False
+            if user.email != new_email:
+                user.email = new_email
+                changed = True
+            if user.username != new_username:
+                user.username = new_username
+                changed = True
+            if changed:
+                user.save()
+        except Member.DoesNotExist:
+            pass
+
+
+def reverse_sync(apps, schema_editor):
+    pass  # No reverse needed
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0004_globalsetting_qr_code'),
+    ]
+
+
+    operations = [
+        migrations.RunPython(sync_emails, reverse_sync),
+    ]
